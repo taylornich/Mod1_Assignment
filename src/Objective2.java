a806ab80ef82c00999e13c153d59d89e438f3fa2
taylornich <166781921+taylornich@users.noreply.github.com>
2025-03-09 21:26:07 -0700

    Module 1 Knowledge Check Assignment

--- src/Objective2.java
+++ src/Objective2.java
@@ -2,11 +2,14 @@ import java.util.Scanner;
 
 public class Objective2 {
     public static void main(String[] args) {
+
+        //Objective 2 Task 1
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter a sentence:");
         String sentence = scanner.nextLine();
 
+        //Objective 2 Task 2
         String[] words = sentence.split(" ");
         StringBuilder reversedSentence = new StringBuilder();
 
@@ -14,6 +17,7 @@ public class Objective2 {
             StringBuilder reversedWord = new StringBuilder(word);
             reversedSentence.append(reversedWord.reverse().toString()).append(" ");
         }
+        //Objective 2 Task 3
         System.out.println("\nReversed sentence:");
         System.out.println(reversedSentence.toString().trim());
 
